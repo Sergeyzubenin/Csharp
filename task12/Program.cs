322580e673830ecb3049c3a2ad32cbbc8b602890
Sergey_Z <zubeninsergej07@mail.ru>
2023-08-13 22:30:15 +0500

    task12

--- task12/Program.cs
+++ task12/Program.cs
@@ -8,4 +8,17 @@
 34, 5 -> некратно, остаток 4
 16, 4 -> кратно
 */
-
+Console.WriteLine("Введите первое число:");
+int number1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2=Convert.ToInt32(Console.ReadLine());
+//int number3=0;
+if (number1%number2==0)
+{
+  Console.WriteLine($"Число {number1} кратно {number2}");
+}
+else
+{
+   int number3= number1 % number2;
+   Console.WriteLine($"Остаток равен {number3}");
+}
